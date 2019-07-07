1b2c00016766814c0ab64a0f96936033f5bebdf3
Brian Wang <brian.x.wang@pwc.com>
2019-07-07 20:48:29 +0800

    bug fixing

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,8 +1,17 @@
 $("#remote .typeahead").autocomplete({
   source: (request, response) => {
     if (request && request.term) {
-      $.getJSON('https://01js17x8hg.execute-api.us-east-2.amazonaws.com/prod?key=' + request.term, data => {
-        response(data.map(v => v.Information));
+      $.ajax({
+        type: "GET",
+        url: 'https://01js17x8hg.execute-api.us-east-2.amazonaws.com/prod',
+        data: {
+          key: request.term
+        },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+          response(data.map(v => v.Information));
+        }
       });
     }
   },
